617b093b115b70c3ee09d5ac223cc906da02d37a
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2015-01-07 14:09:45 +0100

    fixed code

--- Scripts/secondControler_Libor.js
+++ Scripts/secondControler_Libor.js
@@ -1,4 +1,11 @@
 angular.module('app')
+
+    .config(function ($sceProvider) {
+        // Completely disable SCE.  For demonstration purposes only!
+        // Do not use in new projects.
+        $sceProvider.enabled(false);
+    })
+
     .controller('secondControler_Libor', function ($scope) {
         $scope.iframe = {
             show: false,
