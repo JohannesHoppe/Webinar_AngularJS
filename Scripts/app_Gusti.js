e53e731dc4f2e6a744c5c2312bb3c07ce2010957
goosetea <augustincalin@yahoo.com>
2015-01-07 12:21:41 +0100

    2nd view

---
+++ Scripts/app_Gusti.js
@@ -0,0 +1,41 @@
+angular.module('app', ['ngRoute'])
+
+    .config([
+        '$routeProvider', function($routeProvider) {
+
+            $routeProvider
+                .when('/', {
+                    templateUrl: 'Templates/exampleController.html',
+                    controller: 'exampleController'
+                })
+                .when('/gusti', {
+                    templateUrl: 'Templates/anotherView_Gusti.html',
+                    controller: 'anotherViewController'
+                });
+
+        }
+    ]);
+
+
+
+
+
+
+
+
+
+
+/*
+define(['myFirstModule', 'domReady!'], function (myFirstModule) {
+
+    myFirstModule.setColor("blue");
+    myFirstModule.setup();
+
+    // 
+    myFirstModule.$events.on("userDidSomething", function (data) {
+        alert("hallo" + data);
+    });
+
+
+});
+*/
