a8c496f7d75225eafcb023570fbe4e24f5d57678
austy81 <hausterlitz@gmail.com>
2015-01-08 10:04:52 +0100

    insert

--- Day2Task3/JAU/Scripts/layout/customerDetailController.js
+++ Day2Task3/JAU/Scripts/layout/customerDetailController.js
@@ -15,14 +15,35 @@ define([
                 isUserEdited: false
             };
 
+            var newCustomer = function () {
+                $scope.model.isUserEdited = true;
+                $scope.model.customerId = null;
+                $scope.model.customer = {
+                    Id: null,
+                    FirstName: '',
+                    LastName: '',
+                    Mail: ''
+                }
+            }
+
             $scope.loadCurrentCustomer = function () {
+
+                if ($scope.model.customerId < 1) {
+                    newCustomer();
+                    return;
+                }
+
                 $http.get(webApiURL + $scope.model.customerId)
                     .success(function (data, status, headers, config) {
                         $scope.model.customer = data;
+                        if (!data) {
+                            newCustomer();
+                        };
                     })
                     .error(function (data, status, headers, config) {
                         alert('Oups... some error has occured.' + data)
                     });
+
             };
 
             $scope.deleteCustomer = function () {
@@ -37,9 +58,28 @@ define([
             }
 
             $scope.saveCustomer = function () {
-                $http.put(webApiURL + $routeParams.customerId,$scope.model.customer)
+                if (!$scope.model.customer.Id) {
+                    post($scope.model.customer);
+                }
+                else {
+                    put($routeParams.customerId, $scope.model.customer);
+                }
+            }
+
+            var put = function (id, customer) {
+                $http.put(webApiURL + id, customer)
                     .success(function (data, status, headers, config) {
-                        alert('Customer has been successfully deleted!.... sorry this is copy$paste error. His data has been updated.');
+                        alert('Put OK');
+                    })
+                    .error(function (data, status, headers, config) {
+                        alert('Oups... some error has occured.' + data)
+                    });
+            }
+
+            var post = function (customer) {
+                $http.post(webApiURL, customer)
+                    .success(function (data, status, headers, config) {
+                        alert('Posted OK');
                     })
                     .error(function (data, status, headers, config) {
                         alert('Oups... some error has occured.' + data)
