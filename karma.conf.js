3c96030d449b407d4473ff24764cede4734cee1d
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2015-01-08 14:27:01 +0100

    with reporter!

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,5 @@
 module.exports = function(config) {
     config.set({
-
         basePath: 'AngularDemo/Scripts',
         frameworks: ['jasmine', 'requirejs'],
         files: [
@@ -8,6 +7,6 @@ module.exports = function(config) {
             { pattern: '**/*.js', included: false }
         ],
         browsers: ['Chrome'],
-        autoWatch: false
+        reporters: ['nested']
     });
 };
