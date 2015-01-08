50e7af54ac553b1d1efad3eae3a9fad1217a335a
alexej777 <alexej.fuss@commerzbank.com>
2015-01-08 16:57:06 +0100

    refresh

--- AngularDemo/Scripts/layout/customerServiceAlexejSpec.js
+++ AngularDemo/Scripts/layout/customerServiceAlexejSpec.js
@@ -5,28 +5,8 @@
 
         describe('customerService', function () {
 
-
-            var scope, customerServiceMock;
-            var modalDialog;
-
+            
             beforeEach(module('layout'));
-
-            beforeEach(inject(function ($rootScope, $controller, $http) {
-
-                scope = $rootScope.$new();
-
-                customerServicMock = {
-                    refreshCustomers: function () {
-                        return $http.get();
-                    }
-                }
-
-                $controller('exampleController', {
-                    $scope: scope,
-                    customerService: customerServicMock
-                });
-
-            }));
 
             it('should call $http.get in get', inject(function (customerService, $http) {
                 spyOn($http, 'get');
